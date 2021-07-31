cd8f8358ebed8146bd7a53d1d1f1edd0489166a0
gilad517 <88197808+gilad517@users.noreply.github.com>
2021-07-31 15:29:32 +0300

    fixed Iris, added distance (#7)

--- Iris.h
+++ Iris.h
@@ -1,17 +1,18 @@
 #pragma once
-static enum irisType { versicolor, virginica, setosa };
+enum class irisType { Versicolor, Virginica, Setosa, Unknown};
 class Iris
 {
 private:
-	double stemWidth;
-	double stemLength;
-	double petalsWidth;
-	double petalsLength;
-	double distance;
-	irisType type;
+	double m_stemWidth;
+	double m_stemLength;
+	double m_petalsWidth;
+	double m_petalsLength;
+	double m_distance;
+	irisType m_type;
 public:
 	Iris(double stemWidth, double stemLength, double petalsWidth, double petalsLength);
-	void setDistance(Iris iris);
+	void setDistanceTo(Iris iris);
 	static int compare(Iris iris1, Iris iris2);
+	void setType(irisType type);
 	irisType getType();
 };
