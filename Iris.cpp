d5b87a4bd63340d5bff916d26ad10494838de2d5
Gilad <gilad.hananel@gmail.com>
2021-07-31 18:17:22 +0300

    added select, fixed Iris class

--- Iris.cpp
+++ Iris.cpp
@@ -1,33 +1,22 @@
 #include "Iris.h"
 
-	Iris::Iris(double stemWidth, double stemLength, double petalsWidth, double petalsLength) {
-		m_stemWidth = stemWidth;
-		m_stemLength = stemLength;
-		m_petalsWidth = petalsWidth;
-		m_petalsLength = petalsLength;
-		m_distance = 0;
-		m_type = irisType::Unknown;
-	}
-	void Iris::setDistanceTo(Iris iris) {
-		double a = m_stemWidth - iris.m_stemWidth, b = m_stemLength - iris.m_stemLength;
-		double c = m_petalsWidth - iris.m_petalsWidth, d = m_petalsLength - iris.m_petalsLength;
-		m_distance = a * a + b * b + c * c + d * d;
-	}
-	
-	void Iris::setType(irisType type) {
-		m_type = type;
-	}
+Iris::Iris(double stemWidth, double stemLength, double petalsWidth, double petalsLength) {
+	m_stemWidth = stemWidth;
+	m_stemLength = stemLength;
+	m_petalsWidth = petalsWidth;
+	m_petalsLength = petalsLength;
+	m_type = irisType::Unknown;
+}
+double Iris::distanceFrom(Iris iris) {
+	double a = m_stemWidth - iris.m_stemWidth, b = m_stemLength - iris.m_stemLength;
+	double c = m_petalsWidth - iris.m_petalsWidth, d = m_petalsLength - iris.m_petalsLength;
+	return a * a + b * b + c * c + d * d;
+}
 
-	irisType Iris::getType() {
-		return m_type;
-	}
+void Iris::setType(irisType type) {
+	m_type = type;
+}
 
-	int Iris::compare(Iris iris1, Iris iris2) {
-		if (iris1.m_distance < iris2.m_distance) {
-			return -1;
-		}
-		if (iris1.m_distance > iris2.m_distance) {
-			return 1;
-		}
-		return 0;
-	}
+irisType Iris::getType() {
+	return m_type;
+}
