d70f6cda0a8ccba3ffe49a36409c2785682a7948
shoshani777 <ori0586789889@gmail.com>
2021-07-31 22:48:40 +0300

    added a bubble sort and removed distance from iris

--- knn.cpp
+++ knn.cpp
@@ -4,18 +4,21 @@ using namespace std;
 double kPlaceInArray(double* arr, int arrSize, int k);
 int main()
 {
-    fstream classified;
-    classified.open("classified.csv", ios::in);
+	fstream classified;
+	classified.open("classified.csv", ios::in);
 	double arr[] = { 1.3,2.4,5.7,3.5,4.7,5.2,6.8,4.6,3.3 };
 
 }
 double kPlaceInArray(double* arr, int arrSize, int k) {
-	for (int i = arrSize; i > 0; i--)
+	bool flag = true;
+	for (int i = arrSize; i > 0 && flag; i--)
 	{
+		flag = false;
 		for (int j = 0; j < i && j < arrSize - 1; j++)
 		{
 			if (arr[j] > arr[j+1])
 			{
+				flag = true;
 				double temp = arr[j];
 				arr[j] = arr[j + 1];
 				arr[j + 1] = temp;
