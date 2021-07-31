022bfa0f23341bafeeab0e81e32da46eb6e73ddd
Gilad <gilad.hananel@gmail.com>
2021-07-31 14:58:25 +0300

    fixed Iris, added distance

--- Iris.cpp
+++ Iris.cpp
@@ -1,31 +1,33 @@
 #include "Iris.h"
-class Iris
-{
-private:
-	double stemWidth;
-	double stemLength;
-	double petalsWidth;
-	double petalsLength;
-	double distance;
-	irisType type;
-public:
-	Iris(double stemWidth, double stemLength, double petalsWidth, double petalsLength) {
-		this->stemWidth = stemWidth;
-		this->stemLength = stemLength;
-		this->petalsWidth = petalsWidth;
-		this->petalsLength = petalsLength;
+
+	Iris::Iris(double stemWidth, double stemLength, double petalsWidth, double petalsLength) {
+		m_stemWidth = stemWidth;
+		m_stemLength = stemLength;
+		m_petalsWidth = petalsWidth;
+		m_petalsLength = petalsLength;
+		m_distance = 0;
+		m_type = irisType::Unknown;
 	}
-	void setDistance(Iris iris);
-	static int compare(Iris iris1, Iris iris2) {
-		if (iris1.distance < iris2.distance) {
+	void Iris::setDistanceTo(Iris iris) {
+		double a = m_stemWidth - iris.m_stemWidth, b = m_stemLength - iris.m_stemLength;
+		double c = m_petalsWidth - iris.m_petalsWidth, d = m_petalsLength - iris.m_petalsLength;
+		m_distance = a * a + b * b + c * c + d * d;
+	}
+	
+	void Iris::setType(irisType type) {
+		m_type = type;
+	}
+
+	irisType Iris::getType() {
+		return m_type;
+	}
+
+	int Iris::compare(Iris iris1, Iris iris2) {
+		if (iris1.m_distance < iris2.m_distance) {
 			return -1;
 		}
-		if (iris1.distance > iris2.distance) {
+		if (iris1.m_distance > iris2.m_distance) {
 			return 1;
 		}
 		return 0;
 	}
-	irisType getType() {
-		return type;
-	}
-};
